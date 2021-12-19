de3a1ba74777f7e6966305acc6a3bc14a9181426
Ruben <56798357+DerBrezel@users.noreply.github.com>
2021-12-19 14:05:45 +0100

    movement and jump
    s

---
+++ Assets/Scripts/Avatar.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Avatar : MonoBehaviour {
+    private PlayerControls playerControls;
+    private InputAction movement;
+    private CharacterController myCC;
+
+    public float moveSpeed = 2;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private float gravity;
+    private Vector2 moveInput;
+    private bool grounded;
+
+
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        myCC = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable() {
+        movement = playerControls.Player.Movement;
+        movement.Enable();
+
+        playerControls.Player.Jump.performed += DoJump;
+        playerControls.Player.Jump.Enable();
+    }
+
+    private void DoJump(InputAction.CallbackContext obj) {
+        Debug.Log("Jump!");
+    }
+
+    private void OnDisable() {
+        movement.Disable();
+        playerControls.Player.Jump.Disable();
+    }
+
+    private void FixedUpdate() {
+        Debug.Log("Movement Values " + movement.ReadValue<Vector2>());
+    }
+
+    private void Update() {
+        grounded = myCC.isGrounded;
+
+        if (grounded && moveInput.y < 0) {
+            moveInput.y = 0f;
+        }
+
+        Vector2 input = movement.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        myCC.Move(move * Time.deltaTime * moveSpeed);
+
+        //jump
+        if (grounded && playerControls.Player.Jump.triggered) {
+            moveInput.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        moveInput.y += gravity * Time.deltaTime;
+        myCC.Move(moveInput * Time.deltaTime);
+
+
+    }
+}
